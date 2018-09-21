487e7b1631c7f159631cedeae7f12e0f15d85bea
Miranda Cabeceiras <mcabecei@lesley.edu>
2018-09-21 13:45:45 -0700

    fix navigation coding

--- index.php
+++ index.php
@@ -39,8 +39,10 @@
 </article>
 
 <nav>
-        <a href="/index.html/">HTML</a> |
-        <a href="/post-submission.html/">Post-Submission</a> 
+<ul>
+    <li><a href="/index.html/">HTML</a></li>
+    <li><a href="/post-submission.html/">Post-Submission</a></li>
+  </ul> 
         </nav>
 
  <main>

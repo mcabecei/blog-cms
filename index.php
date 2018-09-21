869e250f70a699035f4a025ec23e698a1dacc663
Miranda Cabeceiras <mcabecei@lesley.edu>
2018-09-21 14:02:27 -0700

    save work done in multiple code files

--- index.php
+++ index.php
@@ -1,40 +1,6 @@
-<!DOCTYPE html>
-<html>
- <head>
-    <title>Blog CMS</title>
+<?php
 
-    <style>
-            title {
-                font-size: 40px;
-            }
-            body {
-                background-color: #f3c289;
-            }
-            
-            h1 {
-                color: white;
-                text-align: center;
-            }
-            
-            p {
-                font-family: verdana;
-                font-size: 20px;
-            }
-            </style>
-            
-</head>
-<body>
-  <figure>
- <img src="bakingitems.jpg" alt="Baking" style="width:50%">
- <figcaption>Fig.1 -Baking items, bread, muffins etc. .</figcaption>
-    </figure>
-
-<article>
-    <header>
-      <h1>This is my Blog CMS</h1>
-    <h3>This blog will contain important information about Baking.</h3>
-     <p>These Baking instructions are all created from original recipes.</p>
-    </header>
+?>
     <p>For further information, please click on additional links.</p>
 </article>
 

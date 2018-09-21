24098a49c6f9c543f1b33e637c24930c1600c1fa
Miranda Cabeceiras <mcabecei@lesley.edu>
2018-09-21 13:15:54 -0700

    change type pf file to php

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+ <head>
+    <title>Blog CMS</title>
+
+    <style>
+            title {
+                font-size: 40px;
+            }
+            body {
+                background-color: #f3c289;
+            }
+            
+            h1 {
+                color: white;
+                text-align: center;
+            }
+            
+            p {
+                font-family: verdana;
+                font-size: 20px;
+            }
+            </style>
+            
+</head>
+<body>
+  <figure>
+ <img src="bakingitems.jpg" alt="Baking" style="width:50%">
+ <figcaption>Fig.1 -Baking items, bread, muffins etc. .</figcaption>
+    </figure>
+
+<article>
+    <header>
+      <h1>This is my Blog CMS</h1>
+    <h3>This blog will contain important information about Baking.</h3>
+     <p>These Baking instructions are all created from original recipes.</p>
+    </header>
+    <p>For further information, please click on additional links.</p>
+</article>
+
+<nav>
+        <a href="/index.html/">HTML</a> |
+        <a href="/post-submission.html/">Post-Submission</a> |
+        </nav>
+
+ <main>
+      <h1>Baking Recipes</h1>
+    <p> All of these baking recipes, are recipes submitted by users and made from scratch!</p>
+    
+    <p>An unordered list of Baking Instruction Posts:</p>
+    <ul>
+      <li> <a href="post.html">First Post</a> </li>
+    </ul>
+
+          <section>
+                <h1>Fall Recipes</h1>
+                <p>These baking recipes, are ready to enter your kitchen and give off the famous fall scents. Hand crafted specifically for each fall occassion, these recipes will be a hit in your family!</p>
+              </section>
+              
+              <section>
+                <h1>Winter Recipes</h1>
+                <p>These winter baking recipe instructions, are bundled up and ready for the cold holiday season. From events like, Christmas or New Years, these recipes will have you jumping for joy!</p>
+              </section>
+
+              <footer>
+                    <p>Posted by: Miranda Cabeceiras </p>
+                    <p>Contact information: <a href="mcabecei@lesley.edu">mcabecei@lesley.edu</a>.</p>
+                  </footer>
+
+</body>
+</html>
---
+++ post.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+ <head>
+    <title>Blog CMS</title>
+    <style>
+            title {
+                font-size: 40px;
+            }
+            body {
+                background-color: #7a6e60;
+            }
+            
+            h1 {
+                color: white;
+                text-align: center;
+            }
+            
+            p {
+                font-family: 'Times New Roman', Times, serif;
+                font-size: 20px;
+            }
+            </style>
+            
+</head>
+<body>
+  <figure>
+ <img src="bakingitems.jpg" alt="Baking" style="width:50%">
+ <figcaption>Fig.1 -Baking items, bread, muffins etc. .</figcaption>
+    </figure>
+
+
+<nav>
+        <a href="/index.html/">HTML</a> |
+        <a href="/post-submission.html/">Post-Submission</a> |
+        </nav>
+
+ <main>
+      <h1>Baking Recipe for Chocolate Chip Cookies</h1>
+      <p>Follow below for the Instructions to this Recipe:</p>
+
+      <div style="background-color:indianred">
+            <p>Author: Lorem ipsum dolor sit amet 
+            Date Publishe: consectetur adipiscing elit
+            Post Content: Excepteur sint occaecat cupidatat non proident
+            </p>
+          </div>
+
+      <article>
+        <h1>Baking Time:</h1>
+        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+      </article>
+
+      <article>
+            <h1>Baking Instructions:</h1>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+          </article>
+
+              <footer>
+                    <p>Posted by: Miranda Cabeceiras </p>
+                    <p>Contact information: <a href="mcabecei@lesley.edu">mcabecei@lesley.edu</a>.</p>
+                  </footer>
+
+</body>
+</html>

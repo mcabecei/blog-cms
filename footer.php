17f48068d3f46d77a8724a432b6b5624f362be0b
Miranda Cabeceiras <mcabecei@lesley.edu>
2018-09-21 14:10:42 -0700

    save footer menu coding in new file

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer>
+                    <p>Posted by: Miranda Cabeceiras </p>
+                    <p>Contact information: <a href="mcabecei@lesley.edu">mcabecei@lesley.edu</a>.</p>
+                  </footer>

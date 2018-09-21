818663cfa53284a36f00ff3249a3f59527254a1a
Miranda Cabeceiras <mcabecei@lesley.edu>
2018-09-21 14:44:36 -0700

    add in variable codes

--- part9.php
+++ part9.php
@@ -1,3 +1,9 @@
 <?php
 ini_set('display_errors', 1);     // Show errors for debugging
 ?>
+
+$firstName = "Jane";               // String type variable 
+$lastName = "Doe";                 // String type variable
+$age = 35;                         // Integer type variable 
+$married = true;                   // Boolean type variable
+$childrenNames = array("Alice", "Bob");          // Array type variable

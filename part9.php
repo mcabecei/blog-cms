c0c4d5a15296bde293c7327cc996886c946b6990
Miranda Cabeceiras <mcabecei@lesley.edu>
2018-09-21 15:08:07 -0700

    apply code type to part9

--- part9.php
+++ part9.php
@@ -2,8 +2,14 @@
 ini_set('display_errors', 1);     // Show errors for debugging
 ?>
 
-$firstName = "Jane";               // String type variable 
-$lastName = "Doe";                 // String type variable
-$age = 35;                         // Integer type variable 
-$married = true;                   // Boolean type variable
-$childrenNames = array("Alice", "Bob");          // Array type variable
+$firstName = "Jane";                    // String type variable 
+$lastName = "Doe";                      // String type variable
+$age = 35;                              // Integer type variable 
+$married = true;                        // Boolean type variable
+$childrenNames = array("Alice", "Bob");            // Array type variable
+
+$fullName = $firstName . " " . $lastName;                // Concatenation operator
+$dogYears = $age/7;                           // Arithmetic operator
+$isAdult = $dogYears > 4;                      // Comparison operator
+$isSettled = $isAdult and $married;             // Integer type variable
+

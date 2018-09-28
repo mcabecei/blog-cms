2e12b55818c33d154431417cbc03c7baca9c807b
Miranda Cabeceiras <mcabecei@lesley.edu>
2018-09-28 14:31:51 -0700

    add in second step for coding in part 9 step 9.2

--- index.php
+++ index.php
@@ -21,7 +21,12 @@
             $sql = "SELECT title FROM posts";
             $result = mysqli_query($conn, $sql);
             
-                   
+            $postTitles = array();
+
+            while($row = mysqli_fetch_assoc($result)){
+                array_push($postTitles, $row['title']);
+            }
+                               
         $postTitles = array("Blog Post 1", "Blog Post 2", "Blog Post 3");
             return $postTitles;
         }

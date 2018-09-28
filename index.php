3bda62e818a6c0faf13de95945ea3c5be0182eab
Miranda Cabeceiras <mcabecei@lesley.edu>
2018-09-28 14:36:06 -0700

    add in comment codes

--- index.php
+++ index.php
@@ -16,11 +16,13 @@
  <main>
 <?php 
         function getPostTitlesFromDatabase() {
+             // Get all the post titles from the posts table
             include_once 'db_connect.php';
 
             $sql = "SELECT title FROM posts";
             $result = mysqli_query($conn, $sql);
             
+             // Get each result row as an assoc array, then add title to $postTitles
             $postTitles = array();
 
             while($row = mysqli_fetch_assoc($result)){

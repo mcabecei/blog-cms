73a8726bea0fdeef254bf68f27e7a29992c87b45
Miranda Cabeceiras <mcabecei@lesley.edu>
2018-09-28 14:19:28 -0700

    add in new coding content to post.php

--- post.php
+++ post.php
@@ -12,8 +12,20 @@
  <main>
 <?php
     function getPostDetailsFromDatabase() {
-        // TODO in Module 4
-        // get this data from a database instead of hardcoding it
+          // Get the post title
+    $postTitle = rawurldecode($_GET["title"]);
+
+
+    // Get the post that matches the postTitle
+    include_once 'db_connect.php';
+    $sql = "SELECT * FROM posts WHERE title='" . $postTitle . "'";
+    $result = mysqli_query($conn, $sql);
+
+
+    // Get the first row from the result as an associative array
+    $postDetails = mysqli_fetch_assoc($result);
+    return $postDetails;
+
         $postDetails = array('title' => 'Blog Post 1',
                              'content' => 'My first blog post',
                              'date' => '01/01/2018',

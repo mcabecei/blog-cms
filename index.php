67692fb685ce9c3d257c9e3ac5941b42f65e8ada
Miranda Cabeceiras <mcabecei@lesley.edu>
2018-09-28 14:29:01 -0700

    delete coding

--- index.php
+++ index.php
@@ -16,21 +16,7 @@
  <main>
 <?php 
         function getPostTitlesFromDatabase() {
-          // Get all the post titles from the posts table
-    include_once 'db_connect.php';
-
-    $sql = "SELECT title FROM posts";
-    $result = mysqli_query($conn, $sql);
-    
-     // Get each result row as an assoc array, then add title to $postTitles
-     $postTitles = array();
-     while($row = mysqli_fetch_assoc($result)){
-         array_push($postTitles, $row['title']);
-     }
-     return $postTitles;
-    }
-
-             
+                 
         $postTitles = array("Blog Post 1", "Blog Post 2", "Blog Post 3");
             return $postTitles;
         }

774a21e359b551d3fbce54b2f3a17d88be10abfa
Miranda Cabeceiras <mcabecei@lesley.edu>
2018-10-04 14:06:54 -0700

    add some code php variables

--- hello.js
+++ hello.js
@@ -1,1 +1,9 @@
 alert("Hello World");
+
+var firstName = "Jane";                     // String type variable
+var lastName = "Doe";                       // String type variable
+var age = 35;                               // Number type variable
+var married = true;                         // Boolean type variable
+var childrenNames = ["Alice", "Bob"];       // Array type variable
+var husband = { firstName: "John", lastName: "Doe", age: 35 }    
+                                            // Object type variable

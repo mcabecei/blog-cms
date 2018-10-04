9926c75df3953b0eb03fd1cd50add93bf97e101f
Miranda Cabeceiras <mcabecei@lesley.edu>
2018-10-04 14:09:53 -0700

    add some coding operators

--- hello.js
+++ hello.js
@@ -7,3 +7,8 @@ var married = true;                         // Boolean type variable
 var childrenNames = ["Alice", "Bob"];       // Array type variable
 var husband = { firstName: "John", lastName: "Doe", age: 35 }    
                                             // Object type variable
+
+ var fullName = firstName + " " + lastName;        // Concatenation operator
+ var dogYears = age/7;                             // Arithmetic Operator
+ var isAdult = dogYears > 4;                       // Comparison operator
+ var isSettled = isAdult && married;               // Integer type variable

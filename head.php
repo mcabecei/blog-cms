92364c5da99394e70e9ccd11caa8081f31566b8f
Miranda Cabeceiras <mcabecei@lesley.edu>
2018-10-04 15:53:32 -0700

    add in code for script.js file

--- head.php
+++ head.php
@@ -2,3 +2,7 @@
   src="https://code.jquery.com/jquery-3.3.1.min.js"
   integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
   crossorigin="anonymous"></script>
+
+  <?php
+    include 'head.js';
+?>
